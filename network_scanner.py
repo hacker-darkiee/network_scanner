17128857aba02f16a06a349764c10249eb42e99c
Darkiee <hackerdarkiee@gmail.com>
2020-08-14 13:41:15 +0000

    showing brodcast, arp request and combination of both

--- network_scanner.py
+++ network_scanner.py
@@ -5,9 +5,13 @@ import scapy.all as scapy
 def scan(ip):
     # scapy.arping(ip) implementing this function
     arp_request=scapy.ARP(pdst=ip)
-    print(arp_request.summary())
-    # scapy.ls(arp_request) Checking list ip
+    arp_request.show()
+    # print(arp_request.summary())
     brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    # scapy.ls(brodcast) checking list for mac
-    print(brodcast.summary())
+    brodcast.show()
+    arp_request_brodcast = brodcast/arp_request
+    # print(arp_request_brodcast.summary())
+    arp_request_brodcast.show()
+
+
 scan("192.168.43.166")
