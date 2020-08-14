872be5fe5c4e9631621405f827dfbfbc0ffd0223
Darkiee <hackerdarkiee@gmail.com>
2020-08-14 13:33:13 +0000

    sending ip and mac packet

--- network_scanner.py
+++ network_scanner.py
@@ -3,6 +3,11 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
-
-scan("10.0.2.1")
+    # scapy.arping(ip) implementing this function
+    arp_request=scapy.ARP(pdst=ip)
+    print(arp_request.summary())
+    # scapy.ls(arp_request) Checking list ip
+    brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # scapy.ls(brodcast) checking list for mac
+    print(brodcast.summary())
+scan("192.168.43.166")
