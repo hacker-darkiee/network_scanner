09a36f77afa1f117f8c3203a286bea0319f76598
Darkiee <hackerdarkiee@gmail.com>
2020-08-15 13:26:38 +0000

    finally printing the client list of the same network

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t","--target",dest="target", help="Target IP / IP Range.")
+    option = parser.parse_args()
+    return option
 
 def scan(ip):
     # scapy.arping(ip) implementing this function
@@ -8,7 +15,17 @@ def scan(ip):
     brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_brodcast = brodcast/arp_request
     answered_list = scapy.srp(arp_request_brodcast, timeout=1, verbose=False)[0]
-    print("IP\t\t\tMAC Address\n-----------------------------------------------")
+    clients_list=[]
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-scan("192.168.43.166/24")
+        clients_dict={"ip":element[1].psrc,"mac":element[1].hwsrc}
+        clients_list.append(clients_dict)
+        return clients_list
+
+def print_results(result_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------------")
+    for client in result_list:
+        print(client["ip"]+ "\t\t" + client["mac"])
+
+option = get_arguments
+scan_result = scan("option.target")
+print_results(scan_result)
