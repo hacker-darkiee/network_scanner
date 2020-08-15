a9e13a7b1898576af7d6f430b679124a479a7643
Darkiee <hackerdarkiee@gmail.com>
2020-08-15 09:19:55 +0000

    Results of program enhanced

--- network_scanner.py
+++ network_scanner.py
@@ -7,10 +7,8 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_brodcast = brodcast/arp_request
-    answered_list = scapy.srp(arp_request_brodcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_brodcast, timeout=1, verbose=False)[0]
+    print("IP\t\t\tMAC Address\n-----------------------------------------------")
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("***********************************************")
-
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 scan("192.168.43.166/24")
